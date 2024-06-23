44ae666ad4eda36ef593a1be28938f99247bd374
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-06-23 02:13:47 +0100

    Configured entry file

--- cdk/main.js
+++ cdk/main.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib')
-const { ApiStack } = require('./constructs/api-stack')
+const { UrlShortenerStack } = require('./constructs/url_shortener_stack')
 
 const app = new cdk.App()
-new ApiStack(app, 'ApiStack')
+let stageName = app.node.tryGetContext("stageName");
+
+if (!stageName) {
+  console.log("Defaulting stage name to dev");
+  stageName = "dev";
+}
+
+new UrlShortenerStack(app, `UrlShortenerStack-${stageName}`, {
+    stageName
+})
