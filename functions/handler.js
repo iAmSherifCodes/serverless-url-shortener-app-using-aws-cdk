e01d684d5fc7766c5a32b699037723e70a2c06bb
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-06-27 01:31:36 +0100

    Loaded html template to lambda handler method

--- functions/handler.js
+++ functions/handler.js
@@ -1,8 +1,18 @@
+const mustache = require("mustache");
+const fs = require("fs");
+const shorten_url = process.env.shortenUrl;
+
+const template = fs.readFileSync("static/index.html", "utf-8");
 module.exports.hello = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hi Dear!'
-        })
-    }
+  const html = mustache.render(template, {
+    shorten_url,
+  });
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hi Dear!'
+  })
+  };
+
+  return response;
 };
