21a4b2381bfdd2a598f66ca2d505c6b43720ed3c
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-01 14:18:30 +0100

    Implemented tests cases for handler function

--- tests/test_cases/handler.tests.js
+++ tests/test_cases/handler.tests.js
@@ -1,11 +1,15 @@
-const cheerio = require('cheerio')
+const cheerio = require("cheerio");
+const when =  require('../steps/when')
 
 describe(`When we invoke the GET / endpoint`, () => {
   it(`Should return the index page with contentType text/html`, async () => {
-    const res = await when.we_invoke_get_index()
+    const res = await when.we_invoke_get_index();
 
-    expect(res.statusCode).toEqual(200)
-    expect(res.headers['Content-Type']).toEqual('text/html; charset=UTF-8')
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers["Content-Type"]).toEqual("text/html; charset=UTF-8");
+    expect(res.body).toBeDefined()
 
-  })
-})
+    const _ = cheerio.load(res.body);
+    expect(_).toBeDefined()
+  });
+});
