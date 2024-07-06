e6de2f671b591a9871f3c458287813e4e4d7b59e
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-06 14:29:09 +0100

    Changed test name

--- tests/test_cases/handler.tests.js
+++ tests/test_cases/handler.tests.js
@@ -3,7 +3,7 @@ const when =  require('../steps/when')
 
 describe(`When we invoke the GET / endpoint`, () => {
   it(`Should return the index page with contentType text/html`, async () => {
-    const res = await when.we_invoke_get_index();
+    const res = await when.we_invoke_handler();
 
     expect(res.statusCode).toEqual(200);
     expect(res.headers["Content-Type"]).toEqual("text/html; charset=UTF-8");
