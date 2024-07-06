b84d72662f5d5594b276502f01044ac12324289a
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-06 14:30:44 +0100

    Modifying test case

--- tests/test_cases/shorten_url_test.test.js
+++ tests/test_cases/shorten_url_test.test.js
@@ -1,15 +1,15 @@
-// const { init } = require('../steps/init')
-const when = require('../steps/when')
+const { init } = require("../steps/init");
+const when = require("../steps/when");
 
 describe(`When we invoke the POST /shorten endpoint`, () => {
-//   beforeAll(async () => await init())
+  beforeAll(async () => await init());
 
   it(`Should return short url`, async () => {
-    const res = await when.we_invoke_shorten_url("www.linkedin.com/in/sherifawofiranye")
+    const res = await when.we_invoke_shorten_url(
+      "www.linkedin.com/in/sherifawofiranye"
+    );
 
-    // expect(res.statusCode).toEqual(200)
-    console.log(res.body)
-    // expect(res.body).toBeDefined();
-
-  })
-})
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("url");
+  });
+});
