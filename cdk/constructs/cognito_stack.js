5d702ef73a71b5b70b16592a951f05049ef4fa03
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-16 17:28:43 +0100

    Createed a Cognito stakc for user authentication and implemented stack

---
+++ cdk/constructs/cognito_stack.js
@@ -0,0 +1,49 @@
+const { UserPool, UserPoolClient } = require("aws-cdk-lib/aws-cognito");
+
+class CognitoStack extends Stack {
+  /**
+   * Creates a Cognito User Pool and User Pool Client
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const userPool = new UserPool(this, "UserPool", {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        username: true,
+        email: true,
+      },
+      signInCaseSensitive: false,
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireSymbols: true,
+        requireUppercase: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+    });
+
+    const userPoolClient = new UserPoolClient(this, "WebUserPoolClient", {
+      userPool,
+      generateSecret: false,
+      preventUserExistenceErrors: true,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+    });
+
+    this.userPoolId = userPool.userPoolId;
+    this.userPoolClientId = userPoolClient.userPoolClientId;
+  }
+}
+
+module.exports = {
+  CognitoStack,
+};
