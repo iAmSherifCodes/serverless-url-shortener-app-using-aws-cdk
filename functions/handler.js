a28d22dda0204932e0cf69046f9f994948e37ec8
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-07-19 17:08:12 +0100

    Added dependency to html template from function

--- functions/handler.js
+++ functions/handler.js
@@ -3,8 +3,15 @@ const fs = require("fs");
 const shorten_url = process.env.shortenUrl;
 
 const template = fs.readFileSync("static/index.html", "utf-8");
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+const awsRegion = process.env.AWS_REGION;
+
 module.exports.handler = async (event, context) => {
   const html = mustache.render(template, {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
     shorten_url,
   });
   const response = {
